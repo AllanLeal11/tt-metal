39728e9f9349ddc63da196604d5b12e14a40ac1a
uvelimirovic <uvelimirovic@tenstorrent.com>
2026-04-20 15:04:37 +0000

    Initial commit.

--- tt_metal/api/tt-metalium/tt_backend_api_types.hpp
+++ tt_metal/api/tt-metalium/tt_backend_api_types.hpp
@@ -30,6 +30,7 @@ enum class DataFormat : uint8_t {
     Bfp2_b = 15,
     Lf8 = 10,
     Fp8_e4m3 = 0x1A,
+    MxFp4 = 22,
     Int8 = 14,
     Tf32 = 4,
     UInt8 = 30,
@@ -57,6 +58,7 @@ constexpr static uint32_t datum_size(const DataFormat& format) {
         case DataFormat::Bfp4_b:
         case DataFormat::Bfp8:
         case DataFormat::Bfp8_b: throw std::invalid_argument("datum for bfp2, bfp4, bfp8 is invalid");
+        case DataFormat::MxFp4: throw std::invalid_argument("datum for mxfp4 is invalid");
         case DataFormat::Float16:
         case DataFormat::Float16_b: return 2;
         case DataFormat::Float32: return 4;
@@ -93,6 +95,7 @@ constexpr static uint32_t tile_size(const DataFormat& format) {
         case DataFormat::Bfp4_b: return (128 * 4) + (16 * 4);
         case DataFormat::Bfp8:
         case DataFormat::Bfp8_b: return (256 * 4) + (16 * 4);
+        case DataFormat::MxFp4: return (1024 / 2) + 32;
         case DataFormat::Float16:
         case DataFormat::Float16_b: return (1024 * 2);
         case DataFormat::Float32: return (1024 * 4);
