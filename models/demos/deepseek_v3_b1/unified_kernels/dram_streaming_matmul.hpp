3e6c1cf00b3880c733ae5ecd88bc5d088acf4e93
yugaoTT <yugao@tenstorrent.com>
2026-04-21 17:33:42 +0000

    more fix

--- models/demos/deepseek_v3_b1/unified_kernels/dram_streaming_matmul.hpp
+++ models/demos/deepseek_v3_b1/unified_kernels/dram_streaming_matmul.hpp
@@ -300,13 +300,13 @@ struct DRAMStreamingMatmul {
                         }
                         // Final subblock: finalize=true
                         cb_wait_front(CTArgs::cb_in1, CTArgs::subblock_k);
-                        // custom_mm_block<true>(
-                        //     CTArgs::cb_in0,
-                        //     CTArgs::cb_in1,
-                        //     (CTArgs::num_subblocks_k - 1) * CTArgs::subblock_k,
-                        //     0,
-                        //     0,
-                        //     CTArgs::subblock_k);
+                        custom_mm_block<true>(
+                            CTArgs::cb_in0,
+                            CTArgs::cb_in1,
+                            (CTArgs::num_subblocks_k - 1) * CTArgs::subblock_k,
+                            0,
+                            0,
+                            CTArgs::subblock_k);
                         cb_pop_front(CTArgs::cb_in1, CTArgs::subblock_k);
 
                         tile_regs_commit();
@@ -345,13 +345,13 @@ struct DRAMStreamingMatmul {
                         }
                         // Final subblock: finalize=true
                         cb_wait_front(CTArgs::cb_in1, CTArgs::subblock_k);
-                        // custom_mm_block<true>(
-                        //     CTArgs::cb_in0,
-                        //     CTArgs::cb_in1,
-                        //     (CTArgs::num_subblocks_k - 1) * CTArgs::subblock_k,
-                        //     0,
-                        //     w,
-                        //     CTArgs::subblock_k);
+                        custom_mm_block<true>(
+                            CTArgs::cb_in0,
+                            CTArgs::cb_in1,
+                            (CTArgs::num_subblocks_k - 1) * CTArgs::subblock_k,
+                            0,
+                            w,
+                            CTArgs::subblock_k);
                         cb_pop_front(CTArgs::cb_in1, CTArgs::subblock_k);
                     }
 
