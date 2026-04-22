e885fbc2a0bdbd5f01e9d5bb2f2be1c1d11a8ae9
Nikola Stamatovic <nstamatovic@tenstorrent.com>
2026-04-22 09:24:50 +0000

    feat(bh): add dst_index_out to _llk_math_eltwise_unary_sfpu_params_
    Phase 1: function accepts dst_index_in and dst_index_out but hardware
    is set up using dst_index_in only (same behaviour as before). All
    existing callers will pass the same value for both arguments.

--- tt_metal/tt-llk/tt_llk_blackhole/llk_lib/llk_math_eltwise_unary_sfpu_params.h
+++ tt_metal/tt-llk/tt_llk_blackhole/llk_lib/llk_math_eltwise_unary_sfpu_params.h
@@ -12,11 +12,14 @@
 
 template <typename Callable, typename... Args>
 inline void _llk_math_eltwise_unary_sfpu_params_(
-    Callable&& sfpu_func, std::uint32_t dst_index, int vector_mode = static_cast<int>(VectorMode::RC), Args&&... args)
+    Callable&& sfpu_func, std::uint32_t dst_index_in, std::uint32_t dst_index_out, int vector_mode = static_cast<int>(VectorMode::RC), Args&&... args)
 {
-    LLK_ASSERT((dst_index < get_dest_max_tiles<DST_SYNC_MODE, DST_ACCUM_MODE, DstTileShape::Tile32x32>()), "dst_index exceeds max dest tiles");
+    LLK_ASSERT((dst_index_in < get_dest_max_tiles<DST_SYNC_MODE, DST_ACCUM_MODE, DstTileShape::Tile32x32>()), "dst_index_in exceeds max dest tiles");
+    LLK_ASSERT((dst_index_out < get_dest_max_tiles<DST_SYNC_MODE, DST_ACCUM_MODE, DstTileShape::Tile32x32>()), "dst_index_out exceeds max dest tiles");
 
-    _llk_math_eltwise_unary_sfpu_start_<DST_SYNC_MODE>(dst_index);
+    // Phase 1: hardware addressed from dst_index_in.
+    // Phase 2 TODO: add separate write-address setup when dst_index_in != dst_index_out.
+    _llk_math_eltwise_unary_sfpu_start_<DST_SYNC_MODE>(dst_index_in);
 
     VectorMode mode = static_cast<VectorMode>(vector_mode);
 
