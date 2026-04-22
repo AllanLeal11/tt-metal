9f0c4e286c16541c0dc3622df1234f4af0f53449
Nikola Stamatovic <nstamatovic@tenstorrent.com>
2026-04-22 11:22:17 +0000

    feat(wh-llk): add dst_index_in/out to inner _calculate_*_ callbacks
    All _calculate_*_ functions in tt_llk_wormhole_b0/common/inc/sfpu/ now
    accept (uint dst_index_in, uint dst_index_out) as first parameters and
    use sfpi::dst_reg[idx * SFP_DST_TILE_ROWS] for absolute tile addressing,
    following the binary SFPU callback pattern.

--- tt_metal/tt-llk/tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_square.h
+++ tt_metal/tt-llk/tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_square.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include <cstdint>
+
 #include "sfpi.h"
 
 namespace ckernel
@@ -12,18 +14,14 @@ namespace sfpu
 {
 
 template <bool APPROXIMATION_MODE, int ITERATIONS>
-inline void _calculate_square_()
+inline void _calculate_square_(std::uint32_t dst_index_in, std::uint32_t dst_index_out)
 {
+    constexpr std::uint32_t SFP_DST_TILE_ROWS = 32;
 #pragma GCC unroll 8
     for (int d = 0; d < ITERATIONS; d++)
     {
-        // Load input from destination into LREG0
-        TTI_SFPLOAD(p_sfpu::LREG0, InstrModLoadStore::DEFAULT, ADDR_MOD_3, 0);
-        // Multiply LREG0 * LREG0, store result in LREG0
-        TTI_SFPMUL(p_sfpu::LREG0, p_sfpu::LREG0, p_sfpu::LCONST_0, p_sfpu::LREG0, 0);
-        TTI_SFPNOP;
-        // Store result back to destination
-        TTI_SFPSTORE(p_sfpu::LREG0, InstrModLoadStore::DEFAULT, ADDR_MOD_3, 0);
+        sfpi::vFloat val                                 = sfpi::dst_reg[dst_index_in * SFP_DST_TILE_ROWS];
+        sfpi::dst_reg[dst_index_out * SFP_DST_TILE_ROWS] = val * val;
         sfpi::dst_reg++;
     }
 }
